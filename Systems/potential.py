8d46a191e1cc932c68c28b9a702b435172ac71b1
andrp1 <andrew.petuhov03@yandex.ru>
2025-05-02 19:51:15 +0300

    Main cleanup

--- Systems/potential.py
+++ Systems/potential.py
@@ -1,58 +1,77 @@
 import numpy as np
+from scipy.special import ai_zeros
+
 
 class Potential:
-    def __init__(self):
-        self.potential=None
     @staticmethod
-    def choose_potential():
+    def choose_potential(m: float):
         @staticmethod
-        def choose_oscillator(omega):
+        def choose_oscillator(omega, m: float):
             def oscillator(x):
                 return 0.5 * 1 * (omega ** 2) * x ** 2
-            Potential.potential_type="oscillator"
-            Potential.omega=omega
-            return oscillator
+
+            @staticmethod
+            def theoretical_energies(n: int):
+                return [0.5 * omega * i for i in range(1, n)]
+
+            return oscillator, theoretical_energies
 
         @staticmethod
-        def choose_sech2(a, U0):
+        def choose_sech2(a, U0, m: float):
             def sech2(x):
-                # a = 1
-                # U0 = 14 / 2
                 return -U0 / (np.cosh(x / a)) ** 2
-            Potential.potential_type = "sech2"
-            Potential.U0=U0
-            Potential.a=a
-            return sech2
+
+            @staticmethod
+            def theoretical_energies(n: int):
+                s = 0.5 * (-1 + np.sqrt(1 + 8 * m * U0 * a ** 2))
+                n = np.max([n, int(s - 1e-5) + 1])
+
+                return [float(-(2 * m * a ** 2) ** (-1) * (s - i) ** 2) for i in range(0, n)]
+
+            return sech2, theoretical_energies
 
         @staticmethod
-        def choose_linear(F):
+        def choose_linear(F, m: float):
             def linear(x):
                 if type(x) == float:
                     return F*x if x > 0 else -F*x
 
                 return np.where(x > 0, F*x, -F*x)
 
-            Potential.potential_type = "linear"
-            Potential.F=F
+            @staticmethod
+            def theoretical_energies(n: int):
+                nAiprimezeros = int(-0.1 + n / 2) + 1
+                nAizeros = n - nAiprimezeros
+                Aizeros, Aiprimezeros = ai_zeros(nAiprimezeros)[:2]
 
-            return linear
+                energies = []
+
+                for i in range(nAizeros):
+                    energies.append(float((F ** 2 / (2 * m)) ** (1 / 3) * abs(Aiprimezeros[i])))
+                    energies.append(float((F ** 2 / (2 * m)) ** (1 / 3) * abs(Aizeros[i])))
+                if nAiprimezeros > nAizeros:
+                    energies.append(float((F ** 2 / (2 * m)) ** (1 / 3) * abs(Aiprimezeros[-1])))
+
+                return energies
+
+            return linear, theoretical_energies
 
         print("Выберите тип потенциала: 1 - осциллятор, 2 - sech2, 3 - линейный")
         inp = input()
         if inp == "1":
             print("Введите частоту осциллятора в атомных единицах")
             chosen_omega = float(input())
-            chosen_potential = choose_oscillator(chosen_omega)
+            chosen_potential = choose_oscillator(chosen_omega, m)
         elif inp == "2":
             print("Введите характерное расстояние a в атомных единицах")
             choosen_a = float(input())
             print("Введите глубину потенциала U0 в атомных единицах")
             choosen_U0 = float(input())
-            chosen_potential = choose_sech2(choosen_a, choosen_U0)
+            chosen_potential = choose_sech2(choosen_a, choosen_U0, m)
         elif inp == "3":
             print("Введите силу F>0")
             chosen_F = abs(float(input()))
-            chosen_potential = choose_linear(chosen_F)
+            chosen_potential = choose_linear(chosen_F, m)
         else:
             raise Exception("No potential chosen")
 
--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-import scipy.special
-
 from Models.world_parametrs import WorldParameters as WP
 from Models.hamiltonian import Hamiltonian
 from Systems.single_particle import SingleParticle
 from Visualization.single_particle_visualization import VisualizationSingleParticle1D
 from Systems.potential import Potential
-import numpy as np
 
 
 def main():
-    potentialClass=Potential()
-    potential=potentialClass.choose_potential()
+    particles = SingleParticle()
+    potential, theoretical_energies = Potential().choose_potential(particles.m)
 
-    H = Hamiltonian(particles=SingleParticle(),
+    H = Hamiltonian(particles=particles,
                     potential=potential,
                     N=512, extent=20*WP.Å)
 
@@ -23,28 +20,11 @@ def main():
     if len(eigenstates.energies) == 0:
         print("No states found")
         return
-    m = H.particle_system.m
+
     print("Теоретические значения")
-    theoretical_energies = []
-    if potentialClass.potential_type=="oscillator":
-        theoretical_energies=[0.5*potentialClass.omega*i for i in range(1,len(eigenstates.energies))]
-    if potentialClass.potential_type=="sech2":
-        U0 = potentialClass.U0
-        a = potentialClass.a
-        s=0.5*(-1+np.sqrt(1+8*m*U0*a**2))
-        theoretical_energies=[float(-(2*m*a**2)**(-1) * (s-i)**2) for i in range(0,int(s-(1e-5))+1)]
-    if potentialClass.potential_type=="linear":
-        F=potentialClass.F
-        nAiprimezeros=int(-0.1+len(eigenstates.energies)/2)+1
-        nAizeros=len(eigenstates.energies)-nAiprimezeros
-        Aizeros,Aiprimezeros=scipy.special.ai_zeros(nAiprimezeros)[:2]
-        for i in range(nAizeros):
-            theoretical_energies.append(float((F**2/(2*m))**(1/3)*abs(Aiprimezeros[i])))
-            theoretical_energies.append(float((F**2/(2*m))**(1/3)*abs(Aizeros[i])))
-        if nAiprimezeros>nAizeros:
-            theoretical_energies.append(float((F**2/(2*m))**(1/3)*abs(Aiprimezeros[-1])))
-    for i, x in enumerate(theoretical_energies, 1):
+    for i, x in enumerate(theoretical_energies(n=len(eigenstates.energies)), 1):
         print(f"{x:10}", end="\n" if i % 6 == 0 else " ")
+
     visualization = VisualizationSingleParticle1D(eigenstates, potential)
     visualization.slider_plot()
 
